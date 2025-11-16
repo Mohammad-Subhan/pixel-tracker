3442b23b46f1c39b37b38e2c244e94f1331e7d39
Mohammad Subhan <157013912+Mohammad-Subhan@users.noreply.github.com>
2025-11-16 10:01:26 +0500

    Enhance pixel tracking with Google Sheets integration

--- server.py
+++ server.py
@@ -1,14 +1,80 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, JSONResponse
+import secrets
+import gspread
+from google.oauth2.service_account import Credentials
+from pydantic import BaseModel
+from datetime import datetime
+import pandas as pd
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
 
 app = FastAPI()
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+creds = Credentials.from_service_account_info(
+    info=json.loads(os.environ.get("SERVICE_ACCOUNT_INFO")),
+    scopes=SCOPES,
+)
+
+client = gspread.authorize(creds)
 
 
 @app.get("/track/{tracker_id}")
 def index(tracker_id: str):
-    pixel_path = "pixel.png"
+    sheet = client.open_by_key("1LqMXuRoU8uljeM5MeZkrnVN1rmzNqUh2h1hUhkNsKKQ").sheet1
+    sheet_df = pd.DataFrame(sheet.get_all_records())
+    row_indices = sheet_df.index[sheet_df["Pixel ID"] == tracker_id].tolist()
+    if row_indices:
+        row_index = row_indices[0]
+        sheet_df.at[row_index, "Reads"] += 1
+        sheet_df.at[row_index, "Status"] = (
+            "Read" if sheet_df.at[row_index, "Reads"] > 0 else "Sent"
+        )
+        sheet_df.fillna("", inplace=True)
+        sheet.update([sheet_df.columns.values.tolist()] + sheet_df.values.tolist())
 
-    return FileResponse(
-        pixel_path,
-        media_type="image/png",
+        pixel_path = "pixel.png"
+
+        return FileResponse(
+            pixel_path,
+            media_type="image/png",
+        )
+
+    return JSONResponse(
+        content={
+            "error": "Pixel not found",
+        },
+        status_code=404,
+    )
+
+
+class EmailRequest(BaseModel):
+    email: str
+
+
+@app.post("/track")
+async def track_pixel(request: EmailRequest):
+    email = request.email
+    tracker_id = str(secrets.token_urlsafe(16))
+    pixel_url = f"https://pixel-tracker-xi.vercel.app/track/{tracker_id}"
+
+    new_row = {
+        "Pixel ID": tracker_id,
+        "Email": email,
+        "Status": "Sent",
+        "Reads": 0,
+        "Created At": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+    sheet = client.open_by_key("1LqMXuRoU8uljeM5MeZkrnVN1rmzNqUh2h1hUhkNsKKQ").sheet1
+    sheet.append_row(list(new_row.values()))
+
+    return JSONResponse(
+        content={
+            "id": tracker_id,
+            "trackingUrl": pixel_url,
+        }
     )
